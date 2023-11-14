54e5d620b060e080dd0751d264651db1bf50973b
Angel <angel.storres@alumnos.udg.mx>
2023-11-14 17:47:24 -0600

    main

---
+++ Scripts/main.py
@@ -0,0 +1,9 @@
+from mostrarGraficoEdad import mostrarGraficoEdad
+from mostrarGraficoEdadPrediccion import mostrarGraficoEdadPrediccion
+
+def main():
+	mostrarGraficoEdad(30)
+	mostrarGraficoEdadPrediccion(30)
+
+if __name__ == '__main__':
+	main()
